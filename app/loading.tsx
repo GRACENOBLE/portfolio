162c6d52452cdb0f70afedfc6d79dd59e46432c0
GRACENOBLE <gracenoble72@gmail.com>
2024-05-26 10:28:55 +0300

    positioning loading indicator

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import { LiaCogSolid } from "react-icons/lia";
 
 export default function Loading() {
   return (
-    <div className="absolute z-40 flex w-full h-full  items-center justify-center text-red-600 left-0 top-0 text-6xl">
+    <div className="absolute z-40 flex w-screen h-screen items-center justify-center text-red-600 left-0 top-0 text-6xl">
       <LiaCogSolid className="animate-spin" />
     </div>
   );
