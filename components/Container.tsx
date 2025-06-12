039890a7e0bbd6311c7eacd10e88a57775d58ba3
GRACENOBLE <gracenoble72@gmail.com>
2025-06-12 20:11:38 +0300

    feat(project): delete project

--- components/Container.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Container({children}: {children: React.ReactNode}) {
-  return (
-    <div className='max-w-[1280px] mx-auto pe-10'>
-      {children}
-    </div>
-  )
-}
