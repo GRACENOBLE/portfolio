3cb4859500c6606ee89c13cc4ea8f532a4090dc0
GRACENOBLE <gracenoble72@gmail.com>
2025-06-21 17:46:26 +0300

    feat: add react-icons dependency and update button styles

--- src/components/home/hero-section.tsx
+++ src/components/home/hero-section.tsx
@@ -3,16 +3,23 @@ import { Button } from "../ui/button";
 
 const HeroSection = () => {
   return (
-    <section className="min-h-screen bg-black pt-32">
+    <section className=" bg-black pt-32 border-b">
       <Container className="py-36 text-center text-white flex flex-col items-center">
         <h1 className=" text-7xl font-semibold">I BRING SOFTWARE TO LIFE</h1>
-        <p className="py-10 max-w-2xl">
-          I'm a software developer who turns ideas into scalable, user-focused
-          applications. From backend architecture to pixel-perfect interfaces.
+        <p className="py-10 max-w-3xl text-xl text-muted/60">
+          I'm a software developer who turns ideas into <span className="text-white">scalable, user-focused
+          applications.</span> From backend architecture to pixel-perfect interfaces.
         </p>
         <div className="flex gap-3">
-          <Button size={"lg"}>Start a conversation</Button>
-          <Button size={"lg"}>I need proof</Button>
+          <Button
+            size={"lg"}
+            className="bg-background text-foreground hover:bg-background/70"
+          >
+            Start a conversation
+          </Button>
+          <Button size={"lg"} variant={"outline"}>
+            I need proof
+          </Button>
         </div>
       </Container>
     </section>
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,15 +1,33 @@
 import Container from "../common/container";
 import CustomButton from "../custom-button";
 import Logo from "../logo";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { IoMail } from "react-icons/io5";
+import { Button } from "../ui/button";
 
 const Header = () => {
   return (
     <div className="border-b border-px border-muted/20 fixed top-0 w-full">
       <Container className=" py-4 flex justify-between">
         <Logo variant="wordmark" color="white" />
+        <SocialNav />
       </Container>
     </div>
   );
 };
+
+const SocialNav = () => (
+  <nav className="flex gap-1.5">
+    <Button size={"icon"} variant={"outline"} className="text-white">
+      <FaGithub />
+    </Button>
+    <Button size={"icon"} variant={"outline"} className="text-white">
+      <FaLinkedinIn />
+    </Button>
+    <Button size={"icon"} variant={"outline"} className="text-white">
+      <IoMail />
+    </Button>
+  </nav>
+);
 
 export default Header;
